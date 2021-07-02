63122c1d504f4fb932442b638d77ec453c291218
joshuastjohn718 <jdstjohn1288@gmail.com>
2021-07-02 09:58:52 -0400

    done

---
+++ stopWatch/main.js
@@ -0,0 +1,47 @@
+window.onload = function(){
+    var seconds = 00;
+    var tens = 00;
+    var appendTens = document.getElementById("Tens")
+    var appendSeconds = document.getElementById("seconds")
+    var  ButtonStart = document.getElementById("button-start");
+    var  ButtonStop = document.getElementById("button-stop");
+    var  ButtonReset = document.getElementById("button-restart")
+    var Interval ;
+
+
+   function startTimer() {
+          tens++;
+       if(tens<9){
+        appendTens.innerHTML =="0" + tens;
+       }
+       if(tens>9){
+        appendTens.innerHTML = tens;
+       }
+       if(tens>99){
+           seconds++;
+           appendSeconds.innerHTML ="0" + seconds;
+           tens = 0;
+           appendTens.innerHTML = "0"+ 0;
+       }
+       if(seconds > 9){
+           appendSeconds.innerHTML = seconds;
+       }
+   }
+    ButtonStart.onclick = function(){
+    Interval = setInterval(startTimer);
+  };
+     ButtonStop.onclick = function(){
+        clearInterval(Interval);
+    };
+    
+    ButtonReset.onclick = function(){
+        clearInterval(Interval);
+        tens = "00";
+        seconds = "00";
+        appendSeconds.innerHTML = seconds;
+        appendTens.innerHTML = tens;
+    }
+}
+
+  
+ 
